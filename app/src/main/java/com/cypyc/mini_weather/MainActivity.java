38b5a7a6f950d3254fcaf5f9ce392290895c750c
yuncity <yuncity@pku.edu.cn>
2017-10-18 14:27:54 +0800

    first commit

---
+++ app/src/main/java/com/cypyc/mini_weather/MainActivity.java
@@ -0,0 +1,13 @@
+package com.cypyc.mini_weather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
